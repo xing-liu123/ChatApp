5ec74e6897bff3e45f5ca5cf20fa089e2a87183d
xing-liu123 <LX530189283@gmail.com>
2024-01-29 13:40:21 -0500

    implemented the chatting board

--- ChatApp/Components/MessageBubble.swift
+++ ChatApp/Components/MessageBubble.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MessageBubble: View {
     var message: Message
+    @State private var showtime = false
     
     var body: some View {
         VStack(alignment: message.received ? .leading : .trailing) {
@@ -19,7 +20,23 @@ struct MessageBubble: View {
                                 Color("Gray") : Color("Peach"))
                     .cornerRadius(30)
             }
+            .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showtime.toggle()
+            }
+            
+            if showtime {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                    .padding(message.received ? .leading : .trailing, 25)
+            }
+                
         }
+        .frame(maxWidth: .infinity, alignment:
+                message.received ? .leading : .trailing)
+        .padding(message.received ? .leading : .trailing)
+        .padding(.horizontal, 10 )
     }
 }
 
--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var messageArray = ["Hello you", "How are you doing?", "I've been building SwiftUI applications from scratch and it's so much fun!"]
+    
     var body: some View {
         VStack {
-           TitleRow()
-        }
-        .background(Color("Peach"))
+            TitleRow()
+            
+            ScrollView {
+                ForEach(messageArray, id: \.self) { text in
+                    MessageBubble(message: Message(id:
+                        "12345", text: text, received: true,
+                        timestamp: Date()))
+                    }
+                }
+                .padding(.top, 10)
+                .background(.white)
+                .cornerRadius(30, corners: [.topLeft, .topRight])
+            }
+            .background(Color("Peach"))
     }
 }
 
