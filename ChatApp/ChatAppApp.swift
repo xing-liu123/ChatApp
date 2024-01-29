32fcf26a7ba15ca1cf64704bdc2eaabbad2de3bc
Xavier Liu <u1314373@utah.edu>
2024-01-29 00:00:35 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by xing liu on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
